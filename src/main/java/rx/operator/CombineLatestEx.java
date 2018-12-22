07118626deb10858f691aac40324fd387a9ceeb6
wlsgp012 <wlsgp012@naver.com>
2018-12-22 15:08:26 +0900

    4-29

---
+++ src/main/java/rx/operator/CombineLatestEx.java
@@ -0,0 +1,28 @@
+package rx.operator;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import java.util.concurrent.TimeUnit;
+import rx.common.CommonUtils;
+import rx.common.Log;
+import rx.common.Shape;
+
+public class CombineLatestEx {
+
+  public static void main(String[] args) {
+    String[] data1 = {"6", "7", "4", "2"};
+    String[] data2 = {"DIAMOND", "STAR", "PENTAGON"};
+
+    Observable<String> source = Observable.combineLatest(
+        Observable.fromArray(data1).zipWith(Observable.interval(100L, TimeUnit.MILLISECONDS),
+            (shape, notUsed) -> Shape.getColor(shape)),
+        Observable.fromArray(data2).zipWith(Observable.interval(150L, 200L, TimeUnit.MILLISECONDS),
+            (shape, notUsed) -> Shape.getSuffix(shape)),
+        (v1, v2) -> v1 + v2
+    );
+
+    source.subscribe(Log::i);
+    CommonUtils.sleep(1000);
+  }
+
+}
